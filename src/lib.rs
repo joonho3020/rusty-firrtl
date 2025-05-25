1bb12bf903491ccd320b1848984cae7c4ce86a25
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-05-25 14:49:58 -0700

    Don't print trailing spces when there is no info

--- src/lib.rs
+++ src/lib.rs
@@ -744,10 +744,18 @@ impl Display for Port {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Input(id, tpe, info) => {
-                write!(f, "input {} : {} {}", id, tpe, info)
+                if info == &Info::default() {
+                    write!(f, "input {} : {}", id, tpe)
+                } else {
+                    write!(f, "input {} : {} {}", id, tpe, info)
+                }
             }
             Self::Output(id, tpe, info) => {
-                write!(f, "output {} : {} {}", id, tpe, info)
+                if info == &Info::default() {
+                    write!(f, "output {} : {}", id, tpe)
+                } else {
+                    write!(f, "output {} : {} {}", id, tpe, info)
+                }
             }
         }
     }
